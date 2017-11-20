ebbab9380d9055cad0049c4ad76bf66defa6869b
wzb <zbwang1979@sina.com>
2017-11-20 08:05:56 +0000

    m

--- ifeng_ty_kj.py
+++ ifeng_ty_kj.py
@@ -62,30 +62,23 @@ def log_in(receive_cookie):
     体育TY43 军事JS83
     '''
     comment_url='http://icomment.ifeng.com/wappost.php'
-    # comment_url='http://comment.ifeng.com/post.php'
     my_cookies={item['name']:item['value'] for item in receive_cookie }
     my_s.cookies.update(my_cookies)
-    # r = my_s.get('http://id.ifeng.com', timeout=10)
-    # res_txt = r.text
-    # print(res_txt)
-    # return
-    # ''''KJ123,FOCUSKJ123''''
+
+    # ''''KJ123,FOCUSKJ123 'SYLB10,SYDT10' TY43,FOCUSTY43,TYTOPIC''''
     news_data=[
-        {'news_data': 'KJ123,FOCUSKJ123',
+        {'news_data': 'KJ123',
          'key_word': ' ',
-         'reply_txt': ['清除市场败类酷骑单车，清退用户押金，严惩不法分子，还公平正义给用户。',
-                       '酷骑单车，市场败类']
+         'reply_txt': ['xxxxxxxxxxxx',
+                       'mmmmmmmmmmmmmmm']
          },
-        {'news_data':'TY43,FOCUSTY43,TYTOPIC',
-                'key_word':['中超','中甲','男足','国足','足球','足协','足坛','足联','足总','恒大','上港','权健','鲁能','国安',
-              '富力','亚泰','开新','亿利','申花','辽足','亚冠','郝海东'],
-                'reply_txt':['停止男足💩职业联赛，男足💩退出国际足联，解散男足国家队💩',
-               '中国男足在世界杯出现的唯一办法：和南极洲🐧分在一组，主场定在海南，客场保平，主场争胜,最重要的要请海豹当裁判👮',
-               '中国不适合职业足球，马上解散足协男足，多建几个免费灯光场让普通人玩玩就行了',
-               '停办足球，支援边疆；停办足球，多建绿地；停办足球，学生减负；停办足球，社会将更和谐，大家会更开心',
-               '足协请把你们定位在服务人员上，而不是管理者。创造条件让普通的热爱足球运动的人们能轻松的踢场球，让孩子们能轻松的踢场球，这是你们最该干的',
-               '我现在年薪五六万，真不够用的，听说男足队员都是年薪千万，我现在申请跳槽，加入国足，理由如下： 一、我也能不赢球；二、 我也不能带球突破对方球员；三、对方球员带球也能很轻松的把我晃过；四、面对对方球门，我也射不进球；五、我也接不住队友传给我的球；六、我也不能把球准确地传给队友；七、在场上，我也会吃口香糖；八、在场上我也敢骂裁判；九、输了球，面对媒体我也会潸然泪下，过后又像没事人一样；十、我也爱美女网红女模特，泡吧泡到大天亮；十一、我也有纹身,绝对比贝克汉姆多；十二、我也对豪车如数家珍,有钱就买。十三、输球了我也会发微博，指责骂我们的球迷',
-               '我现在年薪五六万，真不够用的，听说男足队员都是年薪千万，我现在申请跳槽，加入国足，理由如下： 一、我也能不赢球；二、 我也不能带球突破对方球员；三、对方球员带球也能很轻松的把我晃过；四、面对对方球门，我也射不进球；五、我也接不住队友传给我的球；六、我也不能把球准确地传给队友；七、在场上，我也会吃口香糖；八、在场上我也敢骂裁判；九、输了球，面对媒体我也会潸然泪下，过后又像没事人一样；十、我也爱美女网红女模特，泡吧泡到大天亮；十一、我也有纹身,绝对比贝克汉姆多；十二、我也对豪车如数家珍,有钱就买。十三、输球了我也会发微博，指责骂我们的球迷',
+        {'news_data':'TY43',
+                'key_word':['key1',],
+                'reply_txt':['xxxxxxxxxxxx',
+                             'xxxxxxxxxxxx',
+                             'xxxxxxxxxxxx',
+                             'xxxxxxxxxxxx',
+                             'xxxxxxxxxxxx',
                ]},]
     my_s.headers.update({
         'Accept-Encoding': 'gzip, deflate',
@@ -95,14 +88,15 @@ def log_in(receive_cookie):
         'Accept-Language':'zh-CN,zh;q=0.8',
     })
     comment_ext2 = {'comment_level': '0',
+                    'comment_verify':'',
                     'device_type': 'iphone249 plus 1',
                     'from': 'sh',
-                    'isSync': '1',
+                    'isSync': '0',
                     'isTrends': '0',
                     'lat': '39.970585',
                     'location': '丹东鸭绿江',
                     'lon': '123.975259',
-                    'nickname': 'xxxxx',
+                    'nickname': '江边摸鱼人..',
                     'userimg': 'http://my.ifengimg.com/2017/08/02/8326ef78edbd78cf1501652140_1.jpg'
                     }
     try:
@@ -134,31 +128,37 @@ def log_in(receive_cookie):
                     print('清空url_list')
                 if not item['commentsUrl'] in used_url_list:
                     used_url_list.append(item['commentsUrl'])
-                    lead_emoji_list = ['👎', '💩', '👮']
+                    lead_emoji_list = ['👎', '💩']
                     while (True):
                         if len(lead_emoji_list) > 0:
                             my_data = {}
                             my_data['docName'] = item['title']
+                            my_data['quoteId']=''
                             my_data['docUrl'] = item['commentsUrl']
+                            my_data['client'] = '1'
+                            my_data['rt'] = 'sj'
+                            my_data['skey'] = '05BF27'
                             tail_emoji=lead_emoji_list.pop(random.randrange(0,len(lead_emoji_list)))+''.join([re.sub(r":.*?:",'',
                                              emoji.emojize(':%s:'%random.choice(list(emoji_res))))for i in range(5)])
                             reply_message_index=random.randrange(0,len(news_item['reply_txt']))
                             my_data['content'] = news_item['reply_txt'][reply_message_index]+tail_emoji
                             comment_ext2['docId'] = item['commentsUrl']
+                            comment_ext2['docUrl'] ='http://api.iclient.ifeng.com/api_vampire_article_detail?' \
+                                                    'aid=%s&channelid=%s'%(item['commentsUrl'],news_item['news_data'])
+                            comment_ext2['nickname'] = random.choice(['将来买了秘书', '将来买了主席', '将来买了看门大爷', '将来买了保洁阿姨', '将来买了吃瓜群众'])
                             my_data['ext2'] = str(comment_ext2)
                             try:
                                 r = my_s.get(comment_url, params=my_data,timeout=10)
                                 if bool(r):
-                                    res_txt=r.json()
-                                    print('news_%d发布于:%s标题:%s\n状态:%s提示:%s回复:%s' % (len(used_url_list),
-                                    item['updateTime'], item['title'], res_txt['code'],res_txt['message'] if 'message' in res_txt.keys()else ' ',(str(reply_message_index)+tail_emoji)))
+                                    print('news_%d发布于:%s标题:%s\n状态:%s回复:%s' % (len(used_url_list),
+                                    item['updateTime'], item['title'], r.text,(str(reply_message_index)+tail_emoji)))
                                 else:
-                                    print('news_%d发布于:%s标题:%s\n状态:%s提示:%s回复:%s' % (len(used_url_list),item['updateTime'], item['title'], '失败',' ',(str(reply_message_index)+tail_emoji)))
+                                    print('news_%d发布于:%s标题:%s\n状态:%s提示:%s回复:%s' % (len(used_url_list),item['updateTime'], item['title'], r.text,' ',(str(reply_message_index)+tail_emoji)))
                                     break
                             except Exception as e:
                                 print(e)
                                 return
-                            time.sleep(10)
+                            time.sleep(5)
                         else:
                             break
                 else:
